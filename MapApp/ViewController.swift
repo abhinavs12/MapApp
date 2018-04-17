461bb508d4a0b276298c67b4160ce5b8aec4ce79
abhinavs12 <iabhisharma12@gmail.com>
2018-04-16 20:18:07 -0400

    Added default annotation

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    let defaultLatitude = 43.6532
+    var delta = 10.0
+    let defaultLongitude = -79.3832
+    let mapLocation = CLLocationCoordinate2DMake(43.6532, -79.3832)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loc = CLLocationCoordinate2DMake(defaultLatitude, defaultLongitude)
+        let span = MKCoordinateSpanMake(delta, delta)
+        let reg = MKCoordinateRegionMake(loc, span)
+        self.map.region = reg
+        
+        let ann = MKPointAnnotation()
+        ann.coordinate = self.mapLocation
+        ann.title = "Toronto"
+        ann.subtitle = "Awesome Place"
+        
+        self.map.addAnnotation(ann)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
